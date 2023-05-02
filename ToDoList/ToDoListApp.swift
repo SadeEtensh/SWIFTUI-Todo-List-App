e844ee24a205b9a34090f0d5e10ed27dab2f13b2
SadeEtensh <eteneshsade550305@gmail.com>
2023-05-02 16:32:58 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sade on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
