9a37e82fa294c5e221dcbe41714eb35c8a2686e4
SadeEtensh <eteneshsade550305@gmail.com>
2023-05-05 19:40:34 +0300

    competed login Functionality and Field Validation Stage

--- ToDoList/ViewModels/LoginViewViewModel.swift
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Sade on 02/05/2023.
 //
-
+import FirebaseAuth
 import Foundation
 
 class LoginViewViewModel: ObservableObject {
@@ -17,22 +17,29 @@ class LoginViewViewModel: ObservableObject {
     }
     
     func login (){
+        guard validate() else {
+            return
+        }
+        // Try sign in
+        Auth.auth().signIn(withEmail: email, password: password)
+    }
+    
+    
+    
+   private func validate() -> Bool {
+        
         errorMessage = ""
         guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
               !password.trimmingCharacters(in: .whitespaces).isEmpty else {
             errorMessage = "Please fill in all fields."
-            return
+            return false
         }
         
         //Email validation
         guard email.contains("@" ) && email.contains(".") else {
             errorMessage = "Please enter a Valid email."
-            return
+            return false
         }
-    }
-    
-    
-    func validate() {
-        
+        return true
     }
 }
